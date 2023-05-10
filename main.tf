2e60368872678ee792878ee181dc19c1e7a3ec73
BakurD <maxim.bakur@gmail.com>
2023-05-10 04:11:15 +0300

    fixedfffffhgvf

--- main.tf
+++ main.tf
@@ -1,28 +1,14 @@
-
-provider "aws" {
-  // Provider configuration for AWS
-}
-
-provider "ah" {
-  // Provider configuration for AH
-}
-
-provider "alicloud" {
-  // Provider configuration for AliCloud
-}
+provider "{{cookiecutter.aws_provider_region|lower}}" {} 
 
 module "Infrastructure" {
-  source = "{{ cookiecutter.source }}"
+  source = "./infrastructure"
   vpc_cidr_block = "10.0.0.0/16"
   public_subnet_cidr = [
     "10.0.11.0/24",
     "10.0.21.0/24"
   ]
   private_subnet = "10.0.12.0/24"
-  min_size = "{{ cookiecutter.min_size }}"
-  max_size = "{{ cookiecutter.max_size }}"
-  min_elb = "3"
-  vpc_tags = {
-    "Owner" = "Bakurevych"
-  }
+  min_size = "{{cookiecutter.min_size}}"
+  max_size = "{{cookiecutter.max_size}}"
+  min_elb = "5"
 }
